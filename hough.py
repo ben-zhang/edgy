6e072d3a5242681a5081c7b57c0a9d73d386edda
Ben Zhang <benzhang@tribalscale.com>
2019-04-07 21:09:50 -0400

    using hough in edgy isn't likely

--- hough.py
+++ hough.py
@@ -17,25 +17,21 @@ args: image, rho (the p looking thing), theta accuracies (parametric equation of
       threshold(minimum vote for a line)
 """
 
-hough = cv2.HoughLines(edgeMap, 1, np.pi/180, 200)
+hough = cv2.HoughLines(edgeMap, 1, np.pi/180, 500)
+for line in hough:
+  for rho, theta in line:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
 
-for rho, theta in hough[0]:
-  a = np.cos(theta)
-  b = np.sin(theta)
-  x0 = a*rho
-  y0 = b*rho
-  x1 = int(x0 + 1000*(-b))
-  y1 = int(y0 + 1000*(a))
-  x2 = int(x0 - 1000*(-b))
-  y2 = int(y0 - 1000*(a))
+    cv2.line(edgeMap,(x1,y1),(x2,y2),(0,0,255),2)
 
-  cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-
-cv2.imwrite('houghlines3.jpg',img)
-
-lines = cv2.imread('houghlines3.jpg')
-
-cv2.imshow("lines", lines)
+cv2.imshow("lines", edgeMap)
 cv2.waitKey(0)
 
 
